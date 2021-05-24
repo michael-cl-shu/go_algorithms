b240f9f41305b523204c528c3b459dc5f2ca16ce
michael-cl-shu <michael.shu@thermovisionusa.com>
2021-05-24 02:12:22 -0700

    1. Create basePow to simulate math.Pow. 2. Create Char2Decimal to convert a char(rune) to decimal value. 3. Also tried fmt.Sscanf to convert a char(rune) to decimal value. 4. Loop string from left to right. i.e. largest digit to lowest digit. 5. Complete BaseToDec.

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -8,6 +8,35 @@ package module01
 //   BaseToDec("E", 16) => 14
 //   BaseToDec("1110", 2) => 14
 //
+func basePow(base int, pow int) int {
+	product := 1
+
+	for i := 0; i < pow; i++ {
+		product = product * base
+	}
+
+	return product
+}
+
+func Char2Decimal(c rune) int {
+	A := 'A'
+	Zero := '0'
+
+	if c >= '0' && c <= '9' {
+		return int(c) - int(Zero)
+	} else {
+		return (int(c) - int(A)) + 10 // A is 10
+	}
+}
 func BaseToDec(value string, base int) int {
-	return 0
+	sum := 0
+	maxPow := len(value) - 1
+	for i, digit := range value {
+		// var value int
+		// fmt.Sscanf(string(digit), "%X", &value)
+		// sum += value * basePow(base, (maxPow-i))
+		sum += Char2Decimal(digit) * basePow(base, (maxPow-i))
+	}
+
+	return sum
 }
