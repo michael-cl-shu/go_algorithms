88481692cfe6aa5e1c3e3098911f6bbb8d8c4c26
michael-cl-shu <michael.shu@thermovisionusa.com>
2021-05-24 00:37:14 -0700

    1. Complete DecToBase with a recursive function.

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -8,7 +8,18 @@ package module01
 //
 //   DecToBase(14, 16) => "E"
 //   DecToBase(14, 2) => "1110"
-//
+//var digits = []string{"0", "1", "2", "3"}
+
+var digits = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"}
+
 func DecToBase(dec, base int) string {
-	return ""
+
+	if dec > 0 {
+		remainder := dec % base
+		quotient := dec / base
+		return DecToBase(quotient, base) + digits[remainder]
+	} else {
+		// dec == 0
+		return ""
+	}
 }
