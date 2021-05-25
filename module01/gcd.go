d0e342c80d8905812399591528c497cc9f7a17ed
michael-cl-shu <michael.shu@thermovisionusa.com>
2021-05-24 19:11:31 -0700

    1. Complete GCD. Recursive and interative solution.

--- module01/gcd.go
+++ module01/gcd.go
@@ -1,5 +1,21 @@
 package module01
 
+// Given two numbers, A and B:
+
+// Step 1: If B == 0, return A
+// Step 2: A becomes B, and B becomes the remainder of dividing A by B
+// `a, b = b, a % b`
+// Step 3: Go to step 1
+
 func GCD(a, b int) int {
-	return 0
+	// if b == 0 {
+	// 	return a
+	// } else {
+	// 	a, b = b, a%b
+	// 	return GCD(a, b)
+	// }
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
 }
